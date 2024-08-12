ce186791847020ffa4964530f4bf00422ad6e352
akshit raj patel <32068317+akshitrajpatel@users.noreply.github.com>
2024-08-12 11:37:14 +0530

    Update BaseTest.java

--- src/test/BaseTest.java
+++ src/test/BaseTest.java
@@ -14,7 +14,7 @@ public class BaseTest {
     @BeforeEach
     public void setUp() {
         // Set the path for the ChromeDriver
-        System.setProperty("webdriver.chrome.driver", System.getenv("C:\Tools\chromedriver_win32"));
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER_PATH"));
 
         // Initialize ChromeDriver
         driver = new ChromeDriver();
